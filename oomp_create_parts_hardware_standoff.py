6f8a279b78cb74d28d875de5afd9a5c7471d0a67
Oom Lout <aaron@oomlout.com>
2024-01-08 09:48:59 +0000

    changes

--- oomp_create_parts_hardware_standoff.py
+++ oomp_create_parts_hardware_standoff.py
@@ -8,7 +8,7 @@ def load_parts(**kwargs):
     #nylon_white_standoff
     if True:
         extras_sizes = {}
-        extras_sizes["m3"] = [6,9,12,15,20,25,30]
+        extras_sizes["m3"] = [6,9,10,12,15,20,25,30]
         # bolt
         for size in extras_sizes:
             part_details = {}
@@ -18,12 +18,12 @@ def load_parts(**kwargs):
             part_details["size"] = []
             for size in sizes:
                 part_details["size"].append(f"{size}")
-            part_details["color"] = [""]
+            part_details["color"] = ["nylon_white","black","brass"]
             lengths = extras_sizes[size]
             part_details["description_main"] = []
             for length in lengths:
                 part_details["description_main"].append(f"{length}_mm_length")
-            part_details["description_extra"] = "nut_and_nut"
+            part_details["description_extra"] = ["nut_and_nut", "screw_and_nut","screw_and_screw"]
             part_details["manufacturer"] = ""
             part_details["part_number"] = ""
             part_details["kicad_reference"] = ""
