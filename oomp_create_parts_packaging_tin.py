2b1a621ce2e6cea2c6f63be397c969ffd4695681
Oom Lout <aaron@oomlout.com>
2025-03-09 16:09:38 +0000

    Full run through

--- oomp_create_parts_packaging_tin.py
+++ oomp_create_parts_packaging_tin.py
@@ -7,70 +7,90 @@ def load_parts(**kwargs):
     
     parts = []
 
-    
+    part_details = {}
+    part_details["classification"] = "packaging"
+    part_details["type"] = ""
+    part_details["size"] = ""
+    part_details["color"] = ""
+    part_details["description_main"] = ""
+    part_details["description_extra"] = ""
+    part_details["manufacturer"] = ""
+    part_details["part_number"] = ""
+
+    default_empty = part_details.copy()
 
     #define a part 
 
+    #tin_cans
+    if True:
+        part_details = {}        
+        part_details["type"] = "tin_can"
+        part_details["size"] = "diameter_75_mm_height_109_mm_400_ml"
+        parts.append(part_details)
+
+
+
     # tinware tins
-    part_details = {}
-    part_details["classification"] = "packaging"
-    part_details["type"] = "tin_hinged_lid"
-    part_details["size"] = ["169_mm_width_130_mm_height_18_mm_depth_350_ml"]
-    part_details["color"] = [""]
-    part_details["description_main"] = ""
-    part_details["description_extra"] = ""
-    part_details["manufacturer"] = "tinware_direct"
-    part_details["part_number"] = "t4066"        
-    part_details["price_1"] = "£0.79"    
-    part_details["price_10"] = "£0.79"    
-    part_details["price_current"] = "£0.79"
-    part_details["link_purchase"] = ["https://tinwaredirect.com/products/silver-rectangular-stationery-tins-with-hinged-lid?variant=1726837358619"]
-    part_details["width_internal"] = ["165 mm"]
-    part_details["height_internal"] = ["124 mm"]
-    part_details["depth_internal"] = ["18 mm"]
-    parts.append(part_details)    
+    if True:
+        part_details = {}
+        part_details["classification"] = "packaging"
+        part_details["type"] = "tin_hinged_lid"
+        part_details["size"] = ["169_mm_width_130_mm_height_18_mm_depth_350_ml"]
+        part_details["color"] = [""]
+        part_details["description_main"] = ""
+        part_details["description_extra"] = ""
+        part_details["manufacturer"] = "tinware_direct"
+        part_details["part_number"] = "t4066"        
+        part_details["price_1"] = "£0.79"    
+        part_details["price_10"] = "£0.79"    
+        part_details["price_current"] = "£0.79"
+        part_details["link_purchase"] = ["https://tinwaredirect.com/products/silver-rectangular-stationery-tins-with-hinged-lid?variant=1726837358619"]
+        part_details["width_internal"] = ["165 mm"]
+        part_details["height_internal"] = ["124 mm"]
+        part_details["depth_internal"] = ["18 mm"]
+        parts.append(part_details)    
 
 
-    part_details = {}
-    part_details["classification"] = "packaging"
-    part_details["type"] = "tin_hinged_lid"
-    part_details["size"] = ["a5_220_mm_width_160_mm_height_25_mm_depth_450_ml"]
-    part_details["color"] = [""]
-    part_details["description_main"] = ""
-    part_details["description_extra"] = ""
-    part_details["manufacturer"] = "tinware_direct"
-    part_details["part_number"] = "t4005"        
-    part_details["price_1"] = "£1.25"
-    part_details["price_10"] = "£1.25"    
-    part_details["price_current"] = "£1.25"
-    part_details["link_purchase"] = ["https://tinwaredirect.com/products/silver-rectangular-stationery-tins-with-hinged-lid?variant=1726838210587"]
-    part_details["width_internal"] = ["215 mm"]
-    part_details["height_internal"] = ["156 mm"]
-    part_details["depth_internal"] = ["21 mm"]
-    parts.append(part_details)    
+        part_details = {}
+        part_details["classification"] = "packaging"
+        part_details["type"] = "tin_hinged_lid"
+        part_details["size"] = ["a5_220_mm_width_160_mm_height_25_mm_depth_450_ml"]
+        part_details["color"] = [""]
+        part_details["description_main"] = ""
+        part_details["description_extra"] = ""
+        part_details["manufacturer"] = "tinware_direct"
+        part_details["part_number"] = "t4005"        
+        part_details["price_1"] = "£1.25"
+        part_details["price_10"] = "£1.25"    
+        part_details["price_current"] = "£1.25"
+        part_details["link_purchase"] = ["https://tinwaredirect.com/products/silver-rectangular-stationery-tins-with-hinged-lid?variant=1726838210587"]
+        part_details["width_internal"] = ["215 mm"]
+        part_details["height_internal"] = ["156 mm"]
+        part_details["depth_internal"] = ["21 mm"]
+        parts.append(part_details)    
     
 
     #shop bought ones
-
-    # quality street 2024
-    part_details = {}
-    part_details["classification"] = "packaging"
-    part_details["type"] = "tin_plastic"
-    part_details["size"] = ["octagon_225_mm_width_225_mm_height_80_mm_depth_2500_ml"]
-    part_details["color"] = [""]
-    part_details["description_main"] = ""
-    part_details["description_extra"] = ""
-    part_details["manufacturer"] = "quality_street"
-    part_details["part_number"] = "2024"        
-    part_details["price_1"] = ""
-    part_details["price_10"] = ""    
-    part_details["price_current"] = ""
-    part_details["link_purchase"] = [""]
-    part_details["width_internal"] = ["214 mm"]
-    part_details["height_internal"] = ["214 mm"]
-    part_details["depth_internal"] = ["70 mm"]
-    parts.append(part_details)    
-    
+    if True:
+        # quality street 2024
+        part_details = {}
+        part_details["classification"] = "packaging"
+        part_details["type"] = "tin_plastic"
+        part_details["size"] = ["octagon_225_mm_width_225_mm_height_80_mm_depth_2500_ml"]
+        part_details["color"] = [""]
+        part_details["description_main"] = ""
+        part_details["description_extra"] = ""
+        part_details["manufacturer"] = "quality_street"
+        part_details["part_number"] = "2024"        
+        part_details["price_1"] = ""
+        part_details["price_10"] = ""    
+        part_details["price_current"] = ""
+        part_details["link_purchase"] = [""]
+        part_details["width_internal"] = ["214 mm"]
+        part_details["height_internal"] = ["214 mm"]
+        part_details["depth_internal"] = ["70 mm"]
+        parts.append(part_details)    
+        
 
 
     oomp.add_parts(parts, make_files=make_files)
